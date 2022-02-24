486413dce4494e045ffb125a8c0878aad65ad60d
Stephen Wachanga <wachangasteve@gmail.com>
2022-02-24 21:39:46 +0000

    Resolve workspace display.

---
+++ manager_workspace/manager_workspace/doctype/dummy/dummy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, samwel and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class dummy(Document):
+	pass
---
+++ manager_workspace/manager_workspace/doctype/dummy/test_dummy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, samwel and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class Testdummy(unittest.TestCase):
+	pass
